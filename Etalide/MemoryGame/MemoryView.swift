005592864c3548ff0b35fab5d55418de85c47550
alessia-andrisani <alessiaandrisani@virgilio.it>
2022-05-24 11:37:08 +0200

    Created memory view file.

---
+++ Etalide/MemoryGame/MemoryView.swift
@@ -0,0 +1,20 @@
+//
+//  MemoryView.swift
+//  Etalide
+//
+//  Created by Alessia Andrisani on 24/05/22.
+//
+
+import SwiftUI
+
+struct MemoryView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MemoryView_Previews: PreviewProvider {
+    static var previews: some View {
+        MemoryView()
+    }
+}
