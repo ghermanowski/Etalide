2130aee8f80d71d1fff79cc5e6d3d852e166f4a9
Gregor Hermanowski <57409167+ghermanowski@users.noreply.github.com>
2022-05-27 10:46:47 +0200

    Card images are now stored in a separate folder

--- Etalide/Card/Model/ImageManager.swift
+++ Etalide/Card/Model/ImageManager.swift
@@ -9,9 +9,12 @@ import Foundation
 import UIKit
 
 class ImageManager {
+	private static let imagesPath = "Images"
 	static let shared = ImageManager()
 	
 	private init() {}
+	
+	private let fileManager = FileManager.default
 	
 	func find(_ imageName: String) -> UIImage? {
 		guard let fileURL = fileURL(for: imageName) else { return nil }
@@ -36,16 +39,31 @@ class ImageManager {
 	}
 	
 	private func fileURL(for imageName: String) -> URL? {
-		guard let folderURL = try? FileManager.default.url(
+		guard let documentsURL = try? fileManager.url(
 			for: .documentDirectory,
 			in: .userDomainMask,
 			appropriateFor: nil,
 			create: true
 		) else {
+			print("Documents directory not found.")
 			return nil
 		}
 		
-		return folderURL
+		let imagesURL = documentsURL.appendingPathComponent(Self.imagesPath)
+		
+		if !fileManager.fileExists(atPath: imagesURL.relativePath) {
+			do {
+				try fileManager.createDirectory(
+					at: imagesURL,
+					withIntermediateDirectories: false
+				)
+			} catch {
+				print("\(Self.imagesPath) directory could not be created: \(error.localizedDescription)")
+				return nil
+			}
+		}
+		
+		return imagesURL
 			.appendingPathComponent(imageName)
 	}
 }
