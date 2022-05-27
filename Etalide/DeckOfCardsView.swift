f1dd35de26ef9502f0a722c86ad911fb773fd24b
2007riot <2007riot@gmail.com>
2022-05-27 12:48:53 +0200

    Work on the deck popover

--- Etalide/DeckOfCardsView.swift
+++ Etalide/DeckOfCardsView.swift
@@ -11,27 +11,39 @@ struct DeckOfCardsView: View {
     
     let gridItemLayot = Array(repeating: GridItem(.flexible(maximum: UIScreen.main.bounds.width)), count: 4)
     
+    init(_ deck: Deck) {
+        self.deck = deck
+    }
     
+    private let deck: Deck
     
     var body: some View {
         
         LazyVGrid (columns: gridItemLayot) {
-            ForEach(0..<12) { number in
-                ZStack {
-                    Color
-                        .purple
-                    Text("\(number)")
+            if let cards = deck.allCards {
+                ForEach(cards) { card in
+                    CardView(card, in: deck)
+                        .aspectRatio(2/3, contentMode: .fit)
+                        
+                        .padding()
                 }
-                .aspectRatio(2/3, contentMode: .fit)
-                
-                .padding()
             }
+//            ForEach(0..<12) { number in
+//                ZStack {
+//                    Color
+//                        .purple
+//                    Text("\(number)")
+//                }
+//                .aspectRatio(2/3, contentMode: .fit)
+//
+//                .padding()
+//            }
         }
     }
 }
 
 struct DeckOfCardsView_Previews: PreviewProvider {
     static var previews: some View {
-        DeckOfCardsView()
+        DeckOfCardsView(Deck())
     }
 }
