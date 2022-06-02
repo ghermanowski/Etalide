291f50b42d86967cb83261119e82f8504d4ddb03
Gregor Hermanowski <57409167+ghermanowski@users.noreply.github.com>
2022-06-02 16:33:20 +0200

    Added documentation to ImageManager

--- Etalide/Card/Model/ImageManager.swift
+++ Etalide/Card/Model/ImageManager.swift
@@ -6,17 +6,19 @@
 //
 
 import Foundation
-import UIKit
 
+/// Provides access to the Images directory.
 class ImageManager {
 	private static let imagesPath = "Images"
 	
+	// The shared image manager object for the process.
 	static let shared = ImageManager()
 	
 	private init() {}
 	
 	private let fileManager = FileManager.default
 	
+	/// URL to the Images directory.
 	private lazy var imagesURL: URL = {
 		let documentsURL = try! fileManager.url(
 			for: .documentDirectory,
@@ -37,18 +39,10 @@ class ImageManager {
 		return imagesURL
 	}()
 	
-	func find(withName imageName: String) -> UIImage? {
-		let fileURL = fileURL(for: imageName)
-		
-		do {
-			let data = try Data(contentsOf: fileURL)
-			return UIImage(data: data)
-		} catch {
-			print(error.localizedDescription)
-			return nil
-		}
-	}
-	
+	/// Attempts to save a file in the Images directory.
+	/// - Parameters:
+	///   - imageData: The image data.
+	///   - imageName: The name for the image file.
 	func save(_ imageData: Data, withName imageName: String) {
 		let fileURL = fileURL(for: imageName)
 		
@@ -59,6 +53,8 @@ class ImageManager {
 		}
 	}
 	
+	/// Attempts to delete a file from the Images directory.
+	/// - Parameter imageName: The name of the image file that should be deleted.
 	func delete(withName imageName: String) {
 		let fileURL = fileURL(for: imageName)
 		
@@ -69,6 +65,9 @@ class ImageManager {
 		}
 	}
 	
+	/// Creates a URL to the file in the Images directory.
+	/// - Parameter imageName: The name of the image file.
+	/// - Returns: The URL to access the file.
 	func fileURL(for imageName: String) -> URL {
 		imagesURL.appendingPathComponent(imageName)
 	}
