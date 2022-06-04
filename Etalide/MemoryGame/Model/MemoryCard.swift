45c8e3a44338e9b73e8a196e8b9d87b1634e6e1f
Gregor Hermanowski <57409167+ghermanowski@users.noreply.github.com>
2022-06-04 15:56:53 +0200

    Added MemoryCard

---
+++ Etalide/MemoryGame/Model/MemoryCard.swift
@@ -0,0 +1,13 @@
+//
+//  MemoryCard.swift
+//  Etalide
+//
+//  Created by Gregor Hermanowski on 04/06/22.
+//
+
+import Foundation
+
+struct MemoryCard: Equatable, Identifiable {
+	var id: UUID
+	let imageURL: URL
+}
