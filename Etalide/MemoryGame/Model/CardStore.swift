4d903fea7f09bf3c66f6000222edbbecbfd0fe9d
Gregor Hermanowski <57409167+ghermanowski@users.noreply.github.com>
2022-06-04 15:58:07 +0200

    Refactored CardStore logic with MemoryCard

--- Etalide/MemoryGame/Model/CardStore.swift
+++ Etalide/MemoryGame/Model/CardStore.swift
@@ -10,39 +10,26 @@ import SwiftUI
 
 // ViewModel for MemoryGame.
 class CardStore: ObservableObject {
-	@Published var cards: [TestCard] = [card1, card2, card3, card4, card5, card6]
+	init(deck: Deck) {
+		cards = deck.allCards!.map { MemoryCard(id: $0.id!, imageURL: $0.imageURL!) }.shuffled()[0...5].shuffled()
+	}
 	
-	@Published var duplicatedCards: [TestCard] = []
-	@Published var hiddenCards: [TestCard] = []
-	@Published var selectedCards: [TestCard] = []
+	@Published var cards: [MemoryCard]
+	@Published var duplicatedCards: [MemoryCard] = []
+	@Published var hiddenCards: [MemoryCard] = []
+	@Published var selectedCards: [MemoryCard] = []
 	
 	// Creates duplicated cards from the cards array.
 	func createGame() {
-		for card in cards {
-			var newCard = card
-			newCard.id = UUID()
-			duplicatedCards.append(newCard)
-			duplicatedCards.append(card)
-		}
-		
+		var duplicatedCards = cards + cards.map { MemoryCard(id: UUID(), imageURL: $0.imageURL) }
 		duplicatedCards.shuffle()
+		self.duplicatedCards = duplicatedCards
 	}
 	
 	// Check is the cards selected match.
 	func checkPair() {
-		if selectedCards[0].emoji == selectedCards[1].emoji {
-			print("Correct!!!!!")
-			
-			hiddenCards += duplicatedCards.filter { selectedCards[0].emoji == $0.emoji }
+		if selectedCards[0].imageURL == selectedCards[1].imageURL {
+			hiddenCards += duplicatedCards.filter { selectedCards[0].imageURL == $0.imageURL }
 		}
 	}
 }
-
-
-
-let card1 = TestCard(id: UUID(), emoji: "🚀")
-let card2 = TestCard(id: UUID(), emoji: "🚔")
-let card3 = TestCard(id: UUID(), emoji: "🚖")
-let card4 = TestCard(id: UUID(), emoji: "✈️")
-let card5 = TestCard(id: UUID(), emoji: "🚑")
-let card6 = TestCard(id: UUID(), emoji: "🚌")
