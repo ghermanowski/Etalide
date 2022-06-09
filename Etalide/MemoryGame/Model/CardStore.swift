0ca9f5b0ce130d0bb6d71fd26502d18f68e1d4ec
Gregor Hermanowski <57409167+ghermanowski@users.noreply.github.com>
2022-06-09 18:21:24 +0200

    Moved createGame to initialiser

--- Etalide/MemoryGame/Model/CardStore.swift
+++ Etalide/MemoryGame/Model/CardStore.swift
@@ -13,10 +13,13 @@ class CardStore: ObservableObject {
 	init(deck: Deck, difficulty: MemoryDifficulty) {
 		cards = Array(
 			deck.allCards!
-				.map { MemoryCard(id: $0.id!, imageURL: $0.imageURL!) }
 				.shuffled()
 				.prefix(difficulty.amount)
+				.map { MemoryCard(id: $0.id!, imageURL: $0.imageURL!) }
 		)
+		
+		let duplicatedCards = cards + cards.map { MemoryCard(id: UUID(), imageURL: $0.imageURL) }
+		self.duplicatedCards = duplicatedCards.shuffled()
 	}
 	
 	@Published var cards: [MemoryCard]
@@ -24,14 +27,7 @@ class CardStore: ObservableObject {
 	@Published var hiddenCards: [MemoryCard] = []
 	@Published var selectedCards: [MemoryCard] = []
 	
-	// Creates duplicated cards from the cards array.
-	func createGame() {
-		var duplicatedCards = cards + cards.map { MemoryCard(id: UUID(), imageURL: $0.imageURL) }
-		duplicatedCards.shuffle()
-		self.duplicatedCards = duplicatedCards
-	}
-	
-	// Check is the cards selected match.
+	/// Checks if the selected cards match.
 	func checkPair() {
 		if selectedCards[0].imageURL == selectedCards[1].imageURL {
 			hiddenCards += duplicatedCards.filter { selectedCards[0].imageURL == $0.imageURL }
