7971340865e1d59140d07c5a60a760d602fa135a
Gregor Hermanowski <57409167+ghermanowski@users.noreply.github.com>
2022-06-10 12:17:57 +0200

    Improved image saving

--- Etalide/Card/View/CardPopupView.swift
+++ Etalide/Card/View/CardPopupView.swift
@@ -8,27 +8,34 @@
 import SwiftUI
 
 struct CardPopupView: View {
-    init(_ card: Card? = nil, deck: Deck, showPopover: Binding<Bool>) {
-        self.card = card
-        self.deck = deck
-        self._showPopover = showPopover
-        
-        let assetName = card?.assetName != nil ? String(localized: String.LocalizationValue(card!.assetName!)) : nil
-        _cardName = .init(initialValue: assetName ?? card?.name ?? "")
-    }
-    
-    @Environment(\.managedObjectContext) private var moc
-    
-    private let card: Card?
-    private let deck: Deck
-    
-    @Binding var showPopover: Bool
+	init(_ card: Card? = nil, deck: Deck, showPopover: Binding<Bool>) {
+		self.card = card
+		self.deck = deck
+		
+		_showPopover = showPopover
+		
+		if let assetName = card?.assetName,
+		   let name = card?.name,
+		   name == assetName {
+			let localisedName = String(localized: String.LocalizationValue(name))
+			_cardName = State(initialValue: localisedName)
+		} else {
+			_cardName = .init(initialValue: card?.name ?? "")
+		}
+	}
 	
-    @State private var cardName: String
-    @State private var image: UIImage?
-    @State private var showImagePicker = false
-    
-    var body: some View {
+	@Environment(\.managedObjectContext) private var moc
+	
+	private let card: Card?
+	private let deck: Deck
+	
+	@Binding private var showPopover: Bool
+	
+	@State private var cardName: String
+	@State private var image: UIImage?
+	@State private var showImagePicker = false
+	
+	var body: some View {
 		VStack(spacing: 32) {
 			Button {
 				showImagePicker.toggle()
@@ -41,10 +48,9 @@ struct CardPopupView: View {
 						CardImageView(imageURL)
 					} else {
 						Color.accentColor
-							.aspectRatio(3 / 4, contentMode: .fit)
-							.cornerRadius(25)
 					}
 				}
+				.aspectRatio(3 / 4, contentMode: .fit)
 				.cornerRadius(25)
 				.overlay {
 					Label("Choose image", systemImage: "camera.circle.fill")
@@ -69,7 +75,7 @@ struct CardPopupView: View {
 		.padding(.horizontal, 48)
 		.padding(.bottom, 32)
 		.background(Color.white)
-        .cornerRadius(25)
+		.cornerRadius(25)
 		.navigationButtons(alignment: .topLeading, padding: 24) {
 			Button(action: dismiss) {
 				Image(systemName: "chevron.down")
@@ -79,96 +85,98 @@ struct CardPopupView: View {
 		.navigationButtons(alignment: .topTrailing, padding: 24) {
 			if card != nil {
 				Button(role: .destructive, action: deleteCard) {
-                    Image(systemName: "trash")
-                }
+					Image(systemName: "trash")
+				}
 				.buttonStyle(.circle)
-            }
+			}
 			
 			Button(action: saveCard) {
 				Image(systemName: "checkmark")
 			}
 			.buttonStyle(.circle)
-        }
-        .sheet(isPresented: $showImagePicker) {
-            ImagePicker(image: $image)
-        }
+		}
+		.sheet(isPresented: $showImagePicker) {
+			ImagePicker(image: $image)
+		}
 		.frame(maxWidth: .infinity, maxHeight: .infinity)
 		.background(Color.primary.opacity(0.75))
-    }
+	}
 	
 	private func dismiss() {
 		showPopover.toggle()
 	}
-    
-    private func deleteCard() {
+	
+	private func deleteCard() {
 		guard let card = card else { return }
 		
-        let cardID = card.id?.uuidString
-        let cardAssetName = card.assetName
-        
-        moc.delete(card)
-        saveContext()
-        
-        // Preset assets could also be deleted via Card.imageURL.
-        // This is not done because then the presets cannot be restored.
-        if cardAssetName == nil,
-           let cardID = cardID {
-            ImageManager.shared.delete(withName: cardID)
-        }
+		let cardID = card.id?.uuidString
+		let cardAssetName = card.assetName
+		
+		moc.delete(card)
+		saveContext()
+		
+		// Preset assets could also be deleted via Card.imageURL.
+		// This is not done because then the presets cannot be restored.
+		if cardAssetName == nil,
+		   let cardID = cardID {
+			ImageManager.shared.delete(withName: cardID)
+		}
 		
 		dismiss()
-    }
-    
-    private func saveContext() {
-        do {
-            try moc.save()
-        } catch {
-            fatalError(error.localizedDescription)
-        }
-    }
-    
-    /// Saves changes to an existing card or creates a new card.
-    private func saveCard() {
-        // When no image was selected, a new name for the existing card will be saved if it was modified.
-        guard let image = image else {
-            guard let card = card,
-                  card.name != cardName else {
-                return
-            }
-            
-            card.name = cardName
-            saveContext()
-            return
-        }
-        
-        guard let imageData = image.jpegData(compressionQuality: 1) else {
-            print("Could not get image data.")
-            return
-        }
-        
-        guard let card = card else {
-            let newCard = Card(context: moc, name: cardName)
-            newCard.addToDeck(deck)
-            saveContext()
-            
-            ImageManager.shared.save(imageData, withName: newCard.id!.uuidString)
-            return
-        }
-        
-        card.name = cardName
-        // If the card was part of a preset, the assetName needs to be deleted,
-        // so that the new image will be used.
-        card.assetName = nil
-        saveContext()
-        
-        ImageManager.shared.save(imageData, withName: card.id!.uuidString)
+	}
+	
+	private func saveContext() {
+		do {
+			try moc.save()
+		} catch {
+			fatalError(error.localizedDescription)
+		}
+	}
+	
+	/// Saves changes to an existing card or creates a new card.
+	private func saveCard() {
+		// When no image was selected, a new name for the existing card will be saved if it was modified.
+		guard let image = image else {
+			guard let card = card,
+				  card.name != cardName else {
+				return
+			}
+			
+			card.name = cardName
+			saveContext()
+			dismiss()
+			return
+		}
+		
+		guard let imageData = image.jpegData(compressionQuality: 1) else {
+			print("Could not get image data.")
+			return
+		}
+		
+		guard let card = card else {
+			let newCard = Card(context: moc, name: cardName)
+			newCard.addToDeck(deck)
+			saveContext()
+			
+			ImageManager.shared.save(imageData, withName: newCard.id!.uuidString)
+			dismiss()
+			return
+		}
+		
+		card.name = cardName
+		// If the card was part of a preset, the assetName needs to be deleted,
+		// so that the new image will be used.
+		card.assetName = nil
+		saveContext()
+		
+		ImageManager.shared.save(imageData, withName: card.id!.uuidString)
 		
 		dismiss()
-    }
+	}
 }
 
 struct CardPopupView_Previews: PreviewProvider {
-    static var previews: some View {
+	static var previews: some View {
 		CardPopupView(deck: Deck(), showPopover: .constant(true))
-    }
+	}
 }
